9577cd41f8d47edfa290f9c80be8eb2e02b4d625
Gabriel Schultz <gabrielschultz2807@gmail.com>
2024-10-31 17:42:59 -0300

    Fix in select on footer

--- hairsector/lib/widgets/footer_widget.dart
+++ hairsector/lib/widgets/footer_widget.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
-class FooterWidget extends StatelessWidget {
+class FooterWidget extends StatefulWidget {
+  @override
+  _FooterWidgetState createState() => _FooterWidgetState();
+}
+
+class _FooterWidgetState extends State<FooterWidget> {
+  // Variável para armazenar o índice do item selecionado
+  int _selectedIndex = 0;
+
+  // Método para lidar com a mudança de índice
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index; // Atualiza o índice selecionado
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       backgroundColor: Colors.black, // Fundo escuro
-      unselectedItemColor: Colors.white54,
+      unselectedItemColor: const Color.fromARGB(255, 255, 255, 255),
       selectedItemColor: Colors.blueAccent,
+      currentIndex: _selectedIndex, // Define o índice atual
       type: BottomNavigationBarType.fixed,
-      items: const [ // Adicione 'const' aqui para melhorar o desempenho
+      onTap: _onItemTapped, // Adiciona o método para lidar com toques
+      items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
           label: 'Início',
